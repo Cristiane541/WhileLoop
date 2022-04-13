c1099a4ac5897cdba9bb12f5403a4ff2022c5c80
Cristiane541 <crisaraujo541@gmail.com>
2022-04-13 11:36:50 -0300

    While Loop

---
+++ src/br/com/practice/ocfa/oracle/test/Main.java
@@ -0,0 +1,13 @@
+package br.com.practice.ocfa.oracle.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 4;
+        while(i > 0){
+            System.out.println("i value is "+i);
+            i--;
+        }
+        System.out.println("i value after the loop "+i);
+    }
+}
